7d8de8adbc63fcb44cb637c4ba45ba58c124b833
craigcliff <craigcliff@gmail.com>
2022-07-04 07:14:09 +0200

    Updated navbar and logo

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -1,20 +1,15 @@
-<template>
+<template> 
     <nav
       class="navbar is-dark"
       aria-label="main navigation"
       role="navigation"
     >
   <div class="navbar-brand">
-    <a
-      class="navbar-item"
-      href="https://bulma.io"
+    <div
+      class="navbar-item is-size-4 is-family-monospace"
     >
-      <img
-        height="28"
-        src="https://bulma.io/images/bulma-logo.png"
-        width="112"
-      >
-    </a>
+        Notes App
+    </div>
 
     <a
       class="navbar-burger"
@@ -33,49 +28,22 @@
     id="navbarBasicExample"
     class="navbar-menu"
   >
-    <div class="navbar-start">
-      <a class="navbar-item">
-        Home
-      </a>
-
-      <a class="navbar-item">
-        Documentation
-      </a>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
-        </div>
-      </div>
-    </div>
 
     <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
-        </div>
-      </div>
+      <RouterLink
+         class="navbar-item"
+         active-class="is-active"
+         to="/"
+       >
+        Notes
+      </RouterLink>
+      <RouterLink
+         class="navbar-item"
+         active-class="is-active"
+         to="/stats"
+       >
+        Stats
+      </RouterLink>
     </div>
   </div>
 </nav>
