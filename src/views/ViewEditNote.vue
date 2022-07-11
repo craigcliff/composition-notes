3da56b47b550ef07f73c3b4aabfe6f784f86fe76
craigcliff <craigcliff@gmail.com>
2022-07-11 08:06:50 +0200

    Added custom color, placeholder and label props

--- src/views/ViewEditNote.vue
+++ src/views/ViewEditNote.vue
@@ -1,5 +1,35 @@
 <template>
     <div class="edit-note">
-        <h1>Edit Note an id of {{ $route.params.id }}</h1>
+         <AddEditNote
+            v-model="noteContent"
+            bgColor="warning"
+            placeHolder="Edit Note"
+            label="Edit Note"
+            ref="addEditNoteRef"
+        >
+            <template #buttons>
+                 <button
+                    @click="$router.back()"
+                    class="button is-link is-light "
+                >
+                    Cancel
+                </button>
+                <button
+                    class="button is-link has-background-info-dark ml-4"
+                    :disabled="!noteContent"
+                >
+                    Save Note
+                </button>
+            </template>
+        </AddEditNote>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import AddEditNote from '@/components/Notes/AddEditNote.vue'
+
+const noteContent = ref('')
+
+
+</script>
