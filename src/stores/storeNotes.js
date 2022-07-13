e181920826a092361dabb3e18248015958079276
craigcliff <craigcliff@gmail.com>
2022-07-13 16:31:08 +0200

    Deleted  note on firebase

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,6 +1,6 @@
 // stores/counter.js
 import { defineStore } from 'pinia'
-import { collection, onSnapshot, doc, setDoc } from 'firebase/firestore'
+import { collection, deleteDoc, onSnapshot, doc, setDoc } from 'firebase/firestore'
 import { db } from '@/js/firebase'
 
 const notesCollectionRef = collection(db, 'notes')
@@ -40,8 +40,8 @@ export const useStoreNotes = defineStore('storeNotes', {
         content
       })
     },
-    deleteNote(idToDelete) {
-      this.notes = this.notes.filter(note => note.id !== idToDelete)
+    async deleteNote(idToDelete) {
+      await deleteDoc(doc(notesCollectionRef, idToDelete))
     },
     updateNote(id, content) {
       const index = this.notes.findIndex(note => note.id === id)
