12df2b49d568c4558191388c8c6713b1970d5af5
craigcliff <craigcliff@gmail.com>
2022-07-17 10:42:10 +0200

    Auth - Redirect user on auth change (using router within store)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from '@/router'
 
+const pinia = createPinia()
+
+// Allows Pinia to make use of router
+pinia.use(({ store }) => {
+    store.router = markRaw(router)
+})
+
 createApp(App)
-    .use(createPinia())
+    .use(pinia)
     .use(router)
     .mount('#app')
